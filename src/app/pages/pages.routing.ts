44e5f0e4898bd26a22fe1d406d1b1a54a4c46c9a
Daniel <davillarreal40@misena.edu.co>
2022-04-14 23:34:32 -0500

    Creación de observables y promesas

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -6,16 +6,20 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
+import { PromesasComponent } from './promesas/promesas.component';
+import { RxjsComponent } from './rxjs/rxjs.component';
 
 const routes: Routes = [
   {
     path: 'dashboard',
     component: PagesComponent,
     children: [
-      { path: '', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'grafica1', component: Grafica1Component },
-      { path: 'account-settings', component: AccountSettingsComponent },
+      { path: '', component: DashboardComponent, data: { titleParameter: 'Dashboard' }},
+      { path: 'progress', component: ProgressComponent, data: { titleParameter: 'Progress' } },
+      { path: 'grafica1', component: Grafica1Component, data: { titleParameter: 'Grafica #1' } },
+      { path: 'account-settings', component: AccountSettingsComponent, data: { titleParameter: 'Temas' } },
+      { path: 'promesas', component: PromesasComponent, data: { titleParameter: 'Promesas' } },
+      { path: 'rxjs', component: RxjsComponent, data: { titleParameter: 'RxJs' } },
     ]
   },
 ];
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  title?: string;
+  tituloSubscribe$?: Subscription;
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.tituloSubscribe$ = this.getArgumentosRuta().subscribe(({ titleParameter }) => {
+                            this.title = titleParameter;
+                            document.title = `AdminPro - ${titleParameter}`
+                          });
   }
+  ngOnDestroy(): void {
+    this.tituloSubscribe$?.unsubscribe();
+  }
+
+  getArgumentosRuta() {
+    return this.router.events
+      .pipe(
+        filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+        filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data)
+      );
+  }
+
 
 }
